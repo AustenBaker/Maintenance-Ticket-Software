024b41a8c343dfbfa2aa692029abcfb8cef681d4
AustenBaker <austenbaker225@gmail.com>
2020-03-28 10:13:05 -0500

    added export classes for screens

---
+++ project/client/src/screens/UpdateTicket.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {} from 'react-native';
+
+export default class Home extends React.Component{
+  render() {
+    return (
+      <text>temp</text>
+    );
+  }
+}
