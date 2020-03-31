0f1bbd4fe0a78abfa00fca8ef61263278e290f63
golferschneider <schnkyl@gmail.com>
2020-03-30 21:10:58 -0500

    init backend-testing

--- project/server/testing/tests.js
+++ project/server/testing/tests.js
@@ -1,4 +1,17 @@
 'use strict';
 
 const request = require('supertest');
+const app = require('../index')
+
+describe('/login',function() { 
+    it('Login throws no errors', function(done){
+        request(app)
+            .post('/login')
+            .send({username: 'harry', password: '$2b$10$1/1.Uhm9qED2PVUoc6yeLuMBRyOBP1qbNvcjoZZCO2lxeCnVf7tzG'})
+            .expect({success:true}, done)
+    })
+})
+
+
+
 
