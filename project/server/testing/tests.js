3aed42cf04440dbca40edd101646bd346c4f2332
golferschneider <schnkyl@gmail.com>
2020-03-30 21:57:03 -0500

    init testing files

--- project/server/testing/tests.js
+++ project/server/testing/tests.js
@@ -3,14 +3,60 @@
 const request = require('supertest');
 const app = require('../index')
 
-describe('/login',function() { 
-    it('Login throws no errors', function(done){
-        request(app)
-            .post('/login')
-            .send({username: 'harry', password: '$2b$10$1/1.Uhm9qED2PVUoc6yeLuMBRyOBP1qbNvcjoZZCO2lxeCnVf7tzG'})
-            .expect({success:true}, done)
+describe("TESTING CATAGORIES", function() {
+    describe('1. Accounts',function() { 
+        it.skip("testing this")
+        //register
+       /* it('POST /register', function(done){
+            request(app)
+                .post('/register')
+                .send({username: 'testing', password: 'isThisWorking?!'})
+                .expect({success:true}, done)
+        })
+
+        //login
+        it('POST /login', function(done){
+            request(app)
+                .post('/login')
+                .send({username: 'testing', password: 'isThisWorking?!'})
+                .expect({success:true}, done)
+        })
+
+        //logout
+        it('POST /logout', function(done){
+            request(app)
+                .post('/logout')
+                .expect({success:true}, done)
+        })
+
+        //delete account
+        it('Delete', function(done){
+            request(app)
+                .post('/delete')
+                .send({username: 'testing'})
+                .expect({success:true}, done)
+        })
+        
+        //Check if account is still there
+        it('POST /login w/ no account', function(done){
+            request(app)
+                .post('/login')
+                .send({username: 'testing', password: 'isThisWorking?!'})
+                .expect(403, done)
+        })*/
     })
+
+    describe('2. Property',function() { 
+        it.skip("Future Developement")
+        
+    })
+
+    describe('3. Ticket', function() {
+        it.skip("Future Development")
+    })
+
 })
+
 
 
 
