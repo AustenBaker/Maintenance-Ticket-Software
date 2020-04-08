0a720b6f1756788a9dbf291e0410922bc43182a4
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-08 11:58:11 -0500

    Updated User tests to verify default user values
    Updated User tests to verify user with no properties specified will initialize properties to default values properly.

--- project/client/data/__tests__/User-test.js
+++ project/client/data/__tests__/User-test.js
@@ -8,15 +8,15 @@ jest.mock('expo', () => ({
     AppLoading: 'AppLoading',
 }));
 
-describe('User', () => {
+describe(`User`, () => {
     jest.useFakeTimers();
   
     beforeEach(() => {
-      // setup
+      // TODO: test setup
     });
 
     afterEach(() => {
-
+      // TODO: test cleanup
     });
 
       
@@ -30,5 +30,63 @@ describe('User', () => {
       expect(tree).toMatchSnapshot();
     });
 
+    describe(`created with default values`, () => {
+      const tree = renderer.create(<User />).toJSON();
+      test(`has the default first name`, () => {
+        expect(tree.props).toHaveProperty(first_name, `FirstName`);
+      });
+
+      test(`has the default last name`, () => {
+        expect(tree.props).toHaveProperty(last_name, `LastName`);
+      });
+
+      test(`has the default unit assigned`, () => {
+        expect(tree.props).toHaveProperty(units);
+        expect(tree.props.units).arrayContaining([`1703`]);
+        expect(tree.props.units).toHaveLength(1);
+      });
+
+      test(`has the default email address`, () => {
+        expect(tree.props).toHaveProperty(email, `default@CastlebergCommunities.com`);
+      });
+
+      test(`has the default phone number`, () => {
+        expect(tree.props).toHaveProperty(phone, `000-000-0000`);
+      });
+
+      test(`has the default contact preference`, () => {
+        expect(tree.props).toHaveProperty(contact, `email`);
+      });
+
+      test(`has the default entry permission`, () => {
+        expect(tree.props).toHaveProperty(entry_permission, `accompanied`);
+      });
+
+      test(`has the default user type`, () => {
+        expect(tree.props).toHaveProperty(user_type, `resident`);
+      });
+
+      test(`has the default note`, () => {
+        expect(tree.props).toHaveProperty(edit_mode,``);
+      });
+
+      test(`has the default edit mode`, () => {
+        expect(tree.props).toHaveProperty(edit_mode, `false`);
+      });
+
+      test(`has no tickets`, () => {
+        expect(tree.props).toHaveProperty(tickets);
+        expect(tree.props.tickets).toBeUndefined();
+      });
+
+    });
+
+    it(``, () => {
+      // TODO: Add more tests
+    });
+
+    it(``, () => {
+      // TODO: Add more tests
+    });
 
   });
