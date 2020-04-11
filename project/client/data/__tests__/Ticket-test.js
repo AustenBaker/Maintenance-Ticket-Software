7ef5a2b455bfd95ee6c5309bb17ea40f3e7a8403
AustenBaker <austenbaker225@gmail.com>
2020-04-11 11:15:26 -0500

    Created test file for Ticket.js

---
+++ project/client/data/__tests__/Ticket-test.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import * as JestNative from '@testing-library/jest-native';
+import renderer from 'react-test-renderer';
+import Ticket from '../Ticker';
+
+expect.extend({...JestNative})
+
+jest.mock('expo', () => ({
+    AppLoading: 'AppLoading',
+}));
+
+//renders ticket screen
+test('renders correctly', () => {
+    const tree = renderer.create(<Ticket />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+//renders ticket edit screen
+test('renders ticket edit screen correctly', () => {
+    const tree = renderer.create(<Ticket ticket_edit_mode />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
