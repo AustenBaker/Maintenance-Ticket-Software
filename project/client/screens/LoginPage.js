6482359cee0019389ad150bc84fd6395c3eb7385
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-19 18:07:47 -0500

    Update LoginPage.js to implement function => class conversion

--- project/client/screens/LoginPage.js
+++ project/client/screens/LoginPage.js
@@ -6,101 +6,39 @@ import { Appearance, AppearanceProvider } from 'react-native-appearance';
 import { colorScheme } from '../stores';
 import Colors from '../constants/Colors';
 
-//
-// // Un-comment the following to port LoginPage to a class component
-//
-// class LoginPage extends React.PureComponent {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: undefined,
-//       password: undefined
-//     }
-//   }
+class LoginPage extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: undefined,
+      password: undefined
+    }
+  }
 
-//   // Checks for login status once app loads (to login page)
-//   async componentDidMount() {
-//     const res = await fetch('http://127.0.0.1:3001/account/status', {
-//         method: 'POST'
-//     });
-//     const data = await res.json();
-//     // Do stuff here
-//     this.props.UserStore.loggedIn = true;
-//     this.props.navigation.navigate('Profile'); // Or something else
-//   }
+  // Checks for login status once app loads (to login page)
+  async componentDidMount() {
+    const res = await fetch('http://127.0.0.1:3001/account/status', {
+        method: 'POST'
+    });
+    const data = await res.json();
+    // Do stuff here
+    this.props.UserStore.loggedIn = true;
+    this.props.navigation.navigate('Profile'); // Or something else
+  }
 
-//   async handleLogin() {
-//     const res = await fetch('http://127.0.0.1:3001/account/login', {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({ username: this.state.username, password: this.state.password })
-//     });
-//     const data = await res.json();
-//     // Do stuff here
-//     this.props.UserStore.loggedIn = true;
-//     this.props.navigation.navigate('Profile'); // Or something else
-//   }
+  async handleLogin() {
+    const res = await fetch('http://127.0.0.1:3001/account/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username: this.state.username, password: this.state.password })
+    });
+    const data = await res.json();
+    // Do stuff here
+    this.props.UserStore.loggedIn = true;
+    this.props.navigation.navigate('Profile'); // Or something else
+  }
 
-//   render() {
-//     return (
-//       <SafeAreaView style={styles.container, themeContainerStyle}>
-//             <Image source={require('../assets/images/uwcrest.png')}
-//               style={styles.smallImage}/>
-
-//             <Text style={themeLargeTitle}>Welcome Back</Text>
-
-//             <TextInput
-//               placeholder="Username"
-//               placeholderTextColor='#8E8E93'
-//               autoCapitalize="none"
-//               keyboardAppearance={themeKeyboard}
-//               style={themeTextBox}
-//               onChangeText={username => this.setState({ username })}
-//               value={user}
-//             />
-//             <TextInput
-//               secureTextEntry
-//               placeholder="Password"
-//               placeholderTextColor='#8E8E93'
-//               autoCapitalize="none"
-//               keyboardAppearance={themeKeyboard}
-//               style={themeTextBox}
-//               onChangeText={password => this.setState({ password })}
-//               value={pass}
-//             />
-
-//             <Button
-//               onPress={ this.handleLogin }
-//               style={themeLargeTitle}
-//               title="Sign In"
-//               accessibilityLabel="Sign In Button"
-//             />
-
-//             <Button
-//               onPress={() => this.props.navigation.replace('SignUp') }
-//               style={themeBodyText}
-//               title="Don’t have an account?"
-//               accessibilityLabel="Go to Sign Up page"
-//             />
-
-//             <Button
-//               onPress={() => this.props.navigation.replace('Tabs') }
-//               style={themeBodyText}
-//               title="Skip Sign In"
-//               accessibilityLabel="Secret Dev Button to Skip Sign In"
-//             />
-
-//       </SafeAreaView>
-//     );
-//   }
-// }
-
-// may need to pass in navigation?
-export default function LoginScreen({ navigation }) {
-    const [user, onChangeUser] = React.useState('');
-    const [pass, onChangePass] = React.useState('');
-    const colorScheme = new ColorScheme();
-
+  render() {
     let themeContainerStyle =
       colorScheme.theme === 'light' ? styles.iosLightContainer : styles.iosDarkContainer;
     let themeTextBox =
@@ -112,86 +50,58 @@ export default function LoginScreen({ navigation }) {
     let themeKeyboard =
       colorScheme.theme === 'light' ? 'light' : 'dark';
 
-  render (() => {
     return (
-    <SafeAreaView style={styles.container, themeContainerStyle}>
-          <Image source={require('../assets/images/uwcrest.png')}
-            style={styles.smallImage}/>
+      <SafeAreaView style={styles.container, themeContainerStyle}>
+            <Image source={require('../assets/images/uwcrest.png')}
+              style={styles.smallImage}/>
 
-          <Text style={themeLargeTitle}>Welcome Back</Text>
+            <Text style={themeLargeTitle}>Welcome Back</Text>
 
-          <TextInput
-            placeholder="Username"
-            placeholderTextColor='#8E8E93'
-            autoCapitalize="none"
-            keyboardAppearance={themeKeyboard}
-            style={themeTextBox}
-            onChangeText={username => this.setState({ username })}
-            value={this.state.username}
-          />
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            placeholderTextColor='#8E8E93'
-            autoCapitalize="none"
-            keyboardAppearance={themeKeyboard}
-            style={themeTextBox}
-            onChangeText={password => this.setState({ password })}
-            value={this.state.password}
-          />
+            <TextInput
+              placeholder="Username"
+              placeholderTextColor='#8E8E93'
+              autoCapitalize="none"
+              keyboardAppearance={themeKeyboard}
+              style={themeTextBox}
+              onChangeText={username => this.setState({ username })}
+              value={user}
+            />
+            <TextInput
+              secureTextEntry
+              placeholder="Password"
+              placeholderTextColor='#8E8E93'
+              autoCapitalize="none"
+              keyboardAppearance={themeKeyboard}
+              style={themeTextBox}
+              onChangeText={password => this.setState({ password })}
+              value={pass}
+            />
 
-          <Button
-            onPress={() => handleLogin(user, pass) }
-            style={themeLargeTitle}
-            title="Sign In"
-            accessibilityLabel="Sign In Button"
-          />
+            <Button
+              onPress={ this.handleLogin }
+              style={themeLargeTitle}
+              title="Sign In"
+              accessibilityLabel="Sign In Button"
+            />
 
-          <Button
-            onPress={() => navigation.replace('SignUp') }
-            style={themeBodyText}
-            title="Don’t have an account?"
-            accessibilityLabel="Go to Sign Up page"
-          />
+            <Button
+              onPress={() => this.props.navigation.replace('SignUp') }
+              style={themeBodyText}
+              title="Don’t have an account?"
+              accessibilityLabel="Go to Sign Up page"
+            />
 
-          <Button
-            onPress={() => navigation.replace('Tabs') }
-            style={themeBodyText}
-            title="Skip Sign In"
-            accessibilityLabel="Secret Dev Button to Skip Sign In"
-          />
+            <Button
+              onPress={() => this.props.navigation.replace('Tabs') }
+              style={themeBodyText}
+              title="Skip Sign In"
+              accessibilityLabel="Secret Dev Button to Skip Sign In"
+            />
 
-    </SafeAreaView>
+      </SafeAreaView>
     );
-  });
+  }
 }
-
-LoginScreen.navigationOptions = {
-  header: null,
-};
-
-function handleLogin(user, pass) {
-    fetch('http://127.0.0.1:3001/account/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: user, password: pass })
-    }).then(res => res.json()).then(data => {
-        // This is successful login, data = { first, last, username . . .}
-    }).catch(err => {
-        // Error login, error code is err.error
-    })
-}
-
-// User landing page: check login status:
-function componentDidMount() {
-    fetch('/account', { method: 'POST' }).then(res => res.json()).then(data => {
-        // User has logged in before, session cookie is valid
-    }).catch(err => {
-        // User did not login, or session has expired
-        // redirect back to un-authenticated route or login screen
-    })
-}
-
 
 const styles = StyleSheet.create({
   container: {
@@ -257,3 +167,5 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
 });
+
+export default LoginPage;
