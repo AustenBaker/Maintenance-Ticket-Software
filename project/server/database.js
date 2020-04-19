3abed33cc588994d13d8130f8fc431ffc394e01b
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-19 09:30:33 -0500

    Updated account.js and database.js
    account.js: removed router.post and .put functions
    database.js: modified User, Ticket and Property data exports

--- project/server/database.js
+++ project/server/database.js
@@ -8,37 +8,30 @@ mongoose.Promise = require('bluebird');
 const dbURL = 'mongodb+srv://dev:FlrB0VpjgNRWTEQ9@cluster0-gcldf.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log('MongoDB connected!'));
 
-exports.User = mongoose.model('User', new Schema({
-    first: { type: String },
-    last: { type: String },
-    units: { type: Array },
-    username: { type: String, required: true, unique: true },
-    password: { type: String },
-    email: { type: String, unique: true },
-    phone: { type: Number },
-    contactPreference: { type: Number },
-    entryPermissions: { type: String, default: 'ANY' },
-    type: { type: String },
-    note: { type: String },
-    tickets: { type: Array },
-    activate: { type: Boolean, default: true }
-}));
+exports.User = mongoose.model('User', {
+    first: String,
+    last: String,
+    username: String,
+    password: String,
+    email: String,
+    type: String
+});
 
+exports.Ticket = mongoose.model('Ticket', {
+    id: Number,
+    email: String,
+    aptComplex: String,
+    unit: String,
+    issue: String,
+    details: String,
+    emergency: Number,
+    resolvedTime: Number,
+    progress: String,
+    closed: Boolean
+});
 
-exports.Ticket = mongoose.model('Ticket', new Schema({
-    id: { type: Number, unique: true },
-    email: { type: String, unique: true },
-    aptComplex: { type: String },
-    unit: { type: String },
-    issue: { type: String },
-    emergency: { type: Number, default: 0, max: 2 },
-    resolvedTime: { type: Number },
-    progress: { type: String },
-    closed: { type: Boolean, default: false }
-}));
-
-exports.Property = mongoose.model('Property', new Schema({
-    id: { type: Number, unique: true },
-    name: { type: String },
-    location: { type: Object }
-}));
+exports.Property = mongoose.model('Property', {
+    id: Number,
+    name: String,
+    location: Object
+});
