2cefcdbd40eca46852bbc65d70158298de17262f
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-19 22:22:17 -0500

    Updated fetch testing to implement some async tests to verify data retrieval and processing functionality

--- project/client/fetch/testing/testing.js
+++ project/client/fetch/testing/testing.js
@@ -7,68 +7,87 @@ var expect = require('chai').expect
 describe("TESTING CATAGORIES", function() {
     console.log("WORKKSDFLJSDKFJ")
     this.timeout(15000)
-    describe('1. Accounts',function() { 
-        it.skip("testing this")
-       /* //register
-        it('fetch /register', async function(done){
-            const res = await register("testing", "pass32", "Kyle", "Schneider", "golferkid", "@outlook.com","RES")
-            console.log(res)
-            expect(res).toEqual("success")
+    describe('1. Accounts',function() {
+       //register
+        it('fetch POST /account/register', async function(){
+            const res = await userFetch.createAccount("kyle","schn",["parkway","hill crest"],"golfkid",
+                                                "topSecret","kyle2@gmail.com",2624739108,2,"Go for it","res","I swear I am nice",
+                                                [],false)
+            expect(res.username).equal("golfkid")
         })
 
         //login
-        it('POST /login', function(done){
-               // .send({username: 'testing', password: 'isThisWorking?!'})
-        }).skip
+        it('fetch POST /account/login', async function(){
+            const res = await userFetch.handleLogin("golfkid","topSecret")
+            //res.json() gets data from request
+            expect(res.status).equal(200)
+        })
+
+        //update
+        it.skip("fetch POST /account/update")
 
         //logout
-        it('POST /logout', function(done){
-                //.expect(302, done)
-        }).skip
+        it('fetch POST /account/logout', async function(){
+            const res = await userFetch.logout()
+            expect(res.status).equal(200)
+        })
+
+        it('fetch GET /account/getUserFromUsername', async function(){
+            const res = await userFetch.getUserFromUsername("golfkid")
+            expect(res.first).equal('kyle')
+        })
 
         //delete account
-        it('Delete', function(done){
-                //.send({username: 'testing'})
-                //.expect(200, done)
-        }).skip
-        
+        it('fetch DELETE /account/delete', async function(){
+            const res = await userFetch.deleteAccount("golfkid")
+            expect(res.status).equal(200)
+        })
+
         //Check if account is still there
-        it('POST /login w/ no account', function(done){
-                //.send({username: 'testing', password: 'isThisWorking?!'})
-                //.expect(403, done)
-        }).skip*/
+        it.skip('fetch POST /account/login (no account)', function(){
+
+        })
     })
 
-    describe('2. Property',function() { 
+    describe('2. Property',function() {
         it.skip("Future Developement")
-        
+
     })
 
     describe('3. Ticket', async function() {
-        it('Create /ticket/', function(){
-            const res = await ticket.submitTicket("a@a.com","parker way apt","213","Door wont open","jammed some how",false,)
-                //.send({issue: 'testing', emergency: false, resolvedTime: 0, progress: "VIEWED",closed: false })
-               // .expect(200, done)
-        }).skip
-
-        it('GET /ticket/:id', async function(){
-            const res = await ticket.getTicketFromId("1585938940652")
-            expect(res.issue).equal("testing")
+        let newTicketID = ""
+        it('fetch POST /ticket/create', async function(){
+            const res = await ticketFetch.submitTicket("a@a.com","parker way apt","213","Door wont open","jammed some how",false,5,"No progress",false)
+            newTicketID = res.id
+            expect(res.id).to.be.a('number')
         })
-        
-        it.skip("Future Developement")
+
+        // TODO: needs to be implemented in backend
+        it.skip('fetch POST /ticket/update', async function(){
+            const res = await ticketFetch.updateTicket("a@a.com","parker way apt","213","I SWITCHED THIS","jammed some how",true,5,"No progress",false)
+            console.log("CHANGING TICKET")
+            console.log(res)
+            expect(res.id).to.be.a('number')
+        })
+
+        it('fetch GET /ticket/:id', async function(){
+            const res = await ticketFetch.getTicketFromId(newTicketID)
+            expect(res.issue).equal("Door wont open")
+        })
+
+        it("fetch Delete /ticket/delete", async function(){
+            const res = await ticketFetch.deleteTicket(newTicketID)
+            expect(res.status).equal(200)
+        })
+
+        it.skip("Future Development")
 /*
         it("Delete /ticket/", function(done){
                // .delete('/ticket/45681654645')
                // .expect(404,done) //just for the time being
                 //.expect(200,done)
         }).skip*/
-        
+
     })
 
 })
-
-
-
-
-
