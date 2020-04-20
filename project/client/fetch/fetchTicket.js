b782353a147b6548a87289dd144b3d1cb154ea3f
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-19 19:37:48 -0500

    Updated fetchUser.js and fetchTicket.js to match frontend required properties, remove testing log messages and fixed async await

--- project/client/fetch/fetchTicket.js
+++ project/client/fetch/fetchTicket.js
@@ -2,19 +2,20 @@ const fetch = require("node-fetch")
 
 const PATH = 'http://127.0.0.1:3001'
 
+
 /**
  * @param {string} email 
- * @param {string} apt 
+ * @param {string} aptComplex
  * @param {string} unit 
  * @param {string} issue 
  * @param {string} details 
- * @param {boolean} emerg 
+ * @param {boolean} emergency
  * @param {number} resolveTime 
- * @param {string} prog 
+ * @param {string} progress
  * @param {boolean} closed 
  */
 export async function submitTicket(email, aptComplex, unit, issue, details, emergency, resolveTime, progress, closed){
-    const response = fetch(PATH + '/ticket/create', {
+    const response = await fetch(PATH + '/ticket/create', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: email, 
@@ -27,16 +28,18 @@ export async function submitTicket(email, aptComplex, unit, issue, details, emer
                             progress: progress, 
                             closed: closed})
     }).then(res => res.json()).then(data => {
-      console.log("Ticket submition successful")
       return data
     }). catch(err => {
-      console.log("ERROR submitting ticket: " + err)
       return err
     })
     return await response.json()
 };
 
 
+/**
+ * 
+ * @param {number} id 
+ */
 export async function deleteTicket(id){
     const response = fetch(PATH + '/ticket/delete', {
       method: 'DELETE',
@@ -53,13 +56,13 @@ export async function deleteTicket(id){
 
 /**
  * @param {string} email 
- * @param {string} apt 
+ * @param {string} aptComplex
  * @param {string} unit 
  * @param {string} issue 
  * @param {string} details 
- * @param {boolean} emerg 
+ * @param {boolean} emergency
  * @param {number} resolveTime 
- * @param {string} prog 
+ * @param {string} progress
  * @param {boolean} closed 
  */
 export async function updateTicket(email, aptComplex, unit, issue, details, emergency, resolveTime, progress, closed){
@@ -76,36 +79,38 @@ export async function updateTicket(email, aptComplex, unit, issue, details, emer
             progress: progress, 
             closed: closed})
         }).then(res => res.json()).then(data => {
-        console.log("Ticket Update Successful")
         return data
     }). catch(err => {
-        console.log("ERROR updating ticket: " + err)
         return err
     })
     return await response.json()
 }
 
+/**
+ * 
+ * @param {string} email 
+ */
 export async function getTicketsFromEmail(email){ 
     const response = fetch(PATH + '/ticket/' + email, {
         method: 'GET'
     }).then(res => res.json()).then(data => {
-        console.log("Get tickets from email Successful")
         return data
     }). catch(err => {
-        console.log("ERROR getting tickets from email: " + err)
         return err
     })
     return await response.json()
 }
 
+/**
+ * 
+ * @param {number} id 
+ */
 export async function getTicketFromId(id){ 
     const response = await fetch(PATH + '/ticket/' + id, {
         method: 'GET'
     }).then(res => res).then(data => {
-        console.log("Get ticket from id Successful")
         return data;
     }). catch(err => {
-        console.log("ERROR getting ticket from id: " + err)
         return err
     })
     return await response.json();
--- project/client/fetch/fetchUser.js
+++ project/client/fetch/fetchUser.js
@@ -1,88 +1,134 @@
+const fetch = require("node-fetch")
+
 const PATH = 'http://127.0.0.1:3001'
 
 
 /**
  * 
- * @param user username of client
- * @param pass password of client
+ * @param {String} user = Username of user
+ * @param {String} pass = Password of user
  */
 export async function handleLogin(user, pass) {
-    fetch(PATH + '/account/login', {
+    const response = await fetch(PATH + '/account/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: user, password: pass })
     }).then(res => res.json()).then(data => {
         console.log("Login Successful")
+        return data
     }).catch(err => {
         console.log("Error during loggin: " + err)
+        return err
     })
+    console.log(response)
+    return await response
 }
 
 
 export async function logout(){
-    fetch(PATH + '/account/logout', {
+    const response = await fetch(PATH + '/account/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: ""
     }).then(res => res.json()).then(data => {
         console.log("Logout Successful")
+        return data
     }).catch(err => {
         console.log("ERROR during logout: " + err)
+        return err
     })
+    return await response.json()
 }
+
 /**
  * 
- * @param body = {username, password, first, last, usernmae, email, type}
+ * @param {String} first 
+ * @param {String} last 
+ * @param {Array} units 
+ * @param {String} username 
+ * @param {String} password 
+ * @param {String} email 
+ * @param {Number} phone 
+ * @param {Number} contactPreference 
+ * @param {String} entryPermission 
+ * @param {String} type 
+ * @param {String} note 
+ * @param {Array} tickets 
+ * @param {Boolean} activate 
  */
-export async function register(username, password, first, last, username, email, type) {
-    fetch(PATH + '/account/register', {
+export async function register(first, last, units, username, password, email, phone, contactPreference, entryPermission, type, note, tickets, activate) {
+    const response = await fetch(PATH + '/account/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, password: password, first: first, last: last, username: username, email: email, type: type })
+        body: JSON.stringify({ first:first, 
+                                last:last, 
+                                units:units, 
+                                username:username, 
+                                password:password, 
+                                email:email, 
+                                phone:phone, 
+                                contactPreference:contactPreference, 
+                                entryPermission:entryPermission, 
+                                type:type, 
+                                note:note, 
+                                tickets:tickets, 
+                                activate:activate
+                            })
     }).then(res => res.json()).then(data => {
-        console.log("Register Successful")
+        return data
     }).catch(err => {
-        console.log("ERROR during registration: " + err)
+        return err
     })
+    return await response
 }
 
 
 export async function deleteAccount(username){ 
-    fetch(PATH + '/account/delete', {
+    const response = await fetch(PATH + '/account/delete', {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
-        body: username
+        body: { username} 
     }).then(res => res.json()).then(data => {
         console.log("Delete Account Successful")
+        return data
     }).catch(err => {
         console.log("ERROR deleting account: " + err)
+        return err
     })
+    console.log(response)
+    return await response
 }
 
-export export async function getUserFromUsername(username){ 
-    fetch(PATH + '/account/:username', {
+export async function getUserFromUsername(username){ 
+    const response = await fetch(PATH + '/account/:username', {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
         body: username
     }).then(res => res.json()).then(data => {
         console.log("User Found from Username Successful")
+        return data
     }).catch(err => {
         console.log("ERROR getting user from username: " + err)
+        return err
     })
+    return await response.json()
 }
 
 /**
- * @param body = {username, password, first, last, usernmae, email, type}
+ * @param body = {username, password, first, last, email, type}
  */
 
-export async function update(username, password, first, last, username, email, type) {
-    fetch(PATH + '/account/update', {
+export async function update(username, password, first, last, email, type) {
+    const response = await fetch(PATH + '/account/update', {
         method: 'PUT',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: username, password: password, first: first, last: last, username: username, email: email, type: type })
     }).then(res => res.json()).then(data => {
         console.log("Update Successful")
+        return data
     }).catch(err => {
         console.log("Error during update: " + err)
+        return err
     })
+    return await response.json()
 }
