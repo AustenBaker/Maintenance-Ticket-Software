046dd7b254dd0d74383f8d4f2907238ec777da47
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-04-24 22:55:56 -0500

    Added TicketUpdate to project
    Added TicketUpdate data type to project
    TicketUpdate contains:
    timestamp: time of ticket update
    user: email address of user updating ticket
    details: updated information regarding maintenance request

---
+++ project/client/data/TicketUpdate.js
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import { View, Button, Text, TextInput, Picker, StyleSheet } from 'react-native';
+import * as CONSTANTS from '../constants/Reference';
+import { colorScheme, userStore } from '../stores/index';
+
+export default class TicketUpdate extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {...TicketUpdate.defaultProps};
+        for (let key in props) {
+            this.state[key] = this.props[key];
+        }
+        this.toggleEmergency = this.toggleEmergency.bind(this);
+        this.updateTicket = this.updateTicket.bind(this);
+    }
+
+    static defaultProps = {
+        user: 'none',
+        timestamp: 0,
+        details: 'No changes.',
+        edit_mode: false
+    }
+
+    /**
+     * If update changes emergency ticket status, update ticket
+     * emergency status and return boolean value true if ticket
+     * status is now set as an Emergency ticket.
+     *
+     * @return boolean True if ticket is now flagged as an Emergency ticket.
+     */
+    toggleEmergency = () => {
+        return super.toggleEmergency(this);
+    }
+
+    updateTicket = () => {
+        return super.addUpdate(this);
+    }
+    makeUpdate = () => {
+        let content = (
+            <View style={styles.container}>
+              <Text style={styles.ticketUpdateTheme}>
+                <Text>
+                  Ticket Update:
+                  {' '}
+                </Text>
+                <TextInput
+                  label='Details'
+                  placeholder={'Additional information.'}
+                  value={this.state.details}
+                  onValueChange={(value) => this.setState({ details: value})}
+                >
+                </TextInput>
+                <Button
+                  title='Add Update'
+                  label='Update Ticket'
+                  onPress={() => {() => this.setState({
+                      timestamp: Date.now(),
+                      user: userStore.email,
+                      edit_mode: false,
+                    }), () => {
+                        this.updateTicket();
+                        // TODO: Redirect to Ticket display
+                  }}}
+                >
+                  <Text>
+                    Update Ticket
+                  </Text>
+                </Button>
+                <Button
+                  title='Cancel Update'
+                  label='Cancel'
+                  onPress={() => {alert(`TODO: return to ticket details`), () => {
+                      // TODO: unmount aborted ticket update & return to ticket display
+                  }}}
+                >
+                  <Text>
+                    Cancel
+                  </Text>
+                </Button>
+              </Text>
+            </View>
+        );
+        return content;
+    }
+
+    getUpdate = () => {
+        let content = (
+            <View style={styles.container}>
+                <Text style={styles.ticketUpdateTheme}>
+                    <Text>
+                        Updated:
+                        {'  '}
+                        {new Date(this.state.timestamp).toString()}
+                    </Text>
+                    <Text>
+                        By:
+                        {'  '}
+                        {this.state.user}
+                    </Text>
+                </Text>
+                <Text style={styles.ticketUpdateTheme}>
+                    {this.state.details}
+                </Text>
+            </View>
+        );
+        return content;
+    }
+
+    render () {
+        let content = '';
+        if (this.state.edit_mode) {
+            content = this.makeUpdate();
+        } else {
+            content = this.getUpdate();
+        }
+        return content;
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        alignContent: 'stretch',
+        alignItems: 'flex-start',
+        alignSelf: 'center',
+    },
+    ticketUpdateTheme: {
+        fontSize: 16,
+    }
+});
---
+++ project/client/data/__tests__/TicketUpdate-test.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { Text, TextInput } from 'react-native';
+import renderer from 'react-test-renderer';
+import { fireEvent, render, wait, getByPlaceholderText, getByDisplayValue, getByRole, getByLabelText, getByTestId, getByTitle, getByText } from '@testing-library/react-native';
+import * as JestNative from '@testing-library/jest-native';
+import * as CONSTANTS from '../../constants/Reference';
+import Ticket from '../Ticket';
+import TicketUpdate from '../TicketUpdate';
+import { userStore, ticketStore, colorScheme } from '../../stores/index';
+
+
+expect.extend({...JestNative});
+
+jest.disableAutomock();
+
+jest.mock('expo', () => ({
+    AppLoading: 'AppLoading',
+}));
+
+describe(`TicketUpdate`, () => {
+    jest.useFakeTimers();
+
+    beforeEach(() => {
+      // TODO: test setup
+    });
+
+    afterEach(() => {
+      // TODO: test cleanup
+    });
+
+
+    it(`renders the ticket update`, () => {
+      const tree = renderer.create(<TicketUpdate />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+
+    it(`renders the ticket update creation screen`, () => {
+      const tree = renderer.create(<TicketUpdate edit_mode />).toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+});
