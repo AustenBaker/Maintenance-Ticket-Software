28762dcd94ee5729f6843383bc0901c6941f73f9
Kelly Vanderhoff <vanderhoff@wisc.edu>
2020-05-01 21:28:46 -0500

    Updated Reference.js and .gitignore for revisions & additions due to testing and validation
    modified email REGEX
    added username REGEX
    added UNIT_PROPERTIES constant
    changed TICKET_UPDATE_PROPERTIES 'user' => 'userEmail' to avoid confusion with User component
    refactored readableTimestamp function
    refactored validation code
    >implemented isValid({type, data}) to avoid breaking validate while refactoring

--- project/client/constants/Reference.js
+++ project/client/constants/Reference.js
@@ -1,6 +1,10 @@
 // REGEX constant
 export const REGEX = {
 
+    // username pattern:  8 to 32 alphabetical characters
+    // also allow dash, period, underscore and @ connectors
+    USERNAME: /^([a-zA-Z_]|[-\.@]){8,32}$/g,
+
     // name pattern: up to 32 ASCII alphabetical characters
     // also allow dash, period and space connectors
     NAME: /^([a-zA-Z]|[-\. ]){2,32}$/gi,
@@ -8,7 +12,7 @@ export const REGEX = {
     // email pattern:  some(.name)*@site.com
     // where some name & site are any combination of letters
     // and numbers, and the site ends in a 2-3 letter/digit extension
-    EMAIL: /^(([a-zA-Z0-9])+([\.-]{1}([a-zA-Z0-9])+)*[@]{1}([a-zA-Z0-9])+([\.-]{1}([a-zA-Z0-9])+)*\.{1}([a-zA-Z0-9]){2,5}){6,32}$/gi,
+    EMAIL: /^(([a-zA-Z0-9])+([\.-]{1}([a-zA-Z0-9])+)*@{1}([a-zA-Z0-9])+([\.-]{1}([a-zA-Z0-9])+)*\.{1}([a-zA-Z0-9]){2,5}){6,32}$/gi,
 
     // phone pattern:  ###-###-#### where # is a digit 0-9
     PHONE: /^\(?([0-9]{3})\)?[-\. ]?([0-9]{3})[-\. ]?([0-9]{4})$/gi,
@@ -66,6 +70,12 @@ export const PREFERRED_CONTACT = {
     TXT: 'text'
 };
 
+// Unit properties
+export const UNIT_PROPERTIES = [
+    'number',       // Unit number, unique to Property
+    'property',     // Property to which unit belongs
+]
+
 // Properties managed
 export const PROPERTY = {
     WSP: 'Whispering Pines',
@@ -77,7 +87,7 @@ export const PROPERTY = {
 
 export const TICKET_UPDATE_PROPERTIES = [
     'timestamp',    // Number Date timestamp
-    'user',         // User email address
+    'userEmail',         // User email address
     'details',      // Details updating Ticket status
 ];
 
@@ -129,91 +139,196 @@ export const DATE = {
  * @returns {String} D/M/YYYY H:MM:SS AM/PM date format String
  */
 export const readableTimestamp = (timestamp = Date.now()) => {
-    let result = null;
-    if (timestamp > DATE.MAX || timestamp < (-DATE.MAX)) {
-        // invalid timestamp value
-        // TODO: throw error rather than returning null
-        return result;
-    }
-    let time = new Date(timestamp);
-    let morning = false;
-    let hour = time.getHours();
-    // convert hours from 0-23 to 1-12 AM/PM
-    if (hour < 12) {
-        morning = true;
-    }
-    if (hour < 1) {
-        hour = 12;
-    }
-    if (hour > 12) {
-        hour -= 12;
-    }
-    let month = time.getMonth() + 1;
-    let minutes = time.getMinutes();
-    let seconds = time.getSeconds();
-    result = month + '/' + time.getDate() + '/' + time.getFullYear() + ' ';
-    result += hour + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':';
-    result += (seconds < 10 ? '0' + seconds : seconds) + (morning ? ' AM' : ' PM');
-    return result;
-}
+  let result = null;
+  let valid = validate('timestamp', timestamp);
+  if (!valid) {
+    // TODO: Better error handling
+    alert("Invalid timestamp.")
+    return null;
+  }
+  let time = new Date(timestamp);
+  let hour = time.getHours();
+  // convert hours from 0-23 to 1-12 AM/PM
+  let morning = hour < 12;
+  if (hour < 1) {
+    hour = 12;
+  }
+  if (hour > 12) {
+    hour -= 12;
+  }
+  let minutes = time.getMinutes;
+  let seconds = time.getSeconds;
+  result = (time.getMonth + 1) + '/' + time.getDate + '/' + time.getFullYear;
+  result += ' ' + hour + ':' + (minutes < 10 ? '0' + minutes : minutes) + ':';
+  result += (seconds < 10 ? '0' + seconds : seconds) + (morning ? ' AM' : ' PM');
+  return result;
+};
+
+const exists = (value) => {return !(value === undefined || value === null)}
+
+const checkUsername = (value) => {return REGEX.USERNAME.test(value)}
+
+const checkPassword = (value) => {return REGEX.PASSWORD.test(value)}
 
 const checkName = (name) => {return REGEX.NAME.test(name)}
 
 const checkEmail = (email) => {return REGEX.EMAIL.test(email)}
 
+const checkPhone = (value) => {return REGEX.PHONE.test(value)}
+
 const checkMemo = (memo) => {return REGEX.MEMO.test(memo)}
 
 const checkUnit = (unit) => {
-    let valid = !(unit === undefined || unit === null);
-    for (let value of TICKET_UPDATE_PROPERTIES) {
-      valid = valid && (value in unit) && validate(value, unit[value]);
-    }
-    return valid;
+    return exists(unit) && Object.entries(unit).every((value) => (value in UNIT_PROPERTIES && validate(value, unit[value])));
 }
 
 const checkUnitList = (unitList) => {
-    let valid = !(unitList === undefined || unitList === null);
-    for (let unit of unitList) {
-        valid = valid && checkUnit(unit);
-    }
-    return valid;
+    return exists(unitList) && unitList.every((unit) => checkUnit(unit));
+}
+
+const checkUnitNumber = (unitNumber) => {return REGEX.UNIT_NUMBER.test(unitNumber);}
+
+const checkProperty = (location) => {return location in PROPERTY;}
+
+const checkContact = (contact) => {return contact in PREFERRED_CONTACT;}
+
+const checkEntry = (entry) => {return entry in ENTRY_PERMISSION;}
+
+const checkUser = (user) => {
+    return exists(user) && USER_PROPERTIES.every((value) in user) && Object.entries(user).every((key, value) => key in USER_PROPERTIES && isValid({type: key, data: value}))
 }
 
 const checkTicketUpdate = (ticketUpdate) => {
-    let valid = !(ticketUpdate === undefined || ticketUpdate === null);
-    for (let value of TICKET_UPDATE_PROPERTIES) {
-        valid = valid && (value in ticketUpdate) && validate(value, ticketUpdate[value]);
-    }
-    return valid;
+    return exists(ticketUpdate) && Object.entries(ticketUpdate).every((value) => value in TICKET_UPDATE_PROPERTIES && validate(value, ticketUpdate[value]));
 }
 
 const checkTicketUpdateList = (ticketUpdateList) => {
-    let valid = !(ticketUpdateList === undefined || ticketUpdateList === null);
-    for (let ticketUpdate of ticketUpdateList) {
-        valid = valid && checkTicketUpdate(ticketUpdate);
-    }
-    return valid;
+    return exists(ticketUpdateList) && ticketUpdateList.every((ticketUpdate) => checkTicketUpdate(ticketUpdate));
 }
 
 const checkTicket = (ticket) => {
-    let valid = !(ticket === undefined || ticket === null);
-    for (let value of TICKET_PROPERTIES) {
-        valid = valid && (value in ticket) && validate(value, ticket[value]);
-    }
-    return valid;
+    return exists(ticket) && Object.entries(ticket).every((value) => value in TICKET_PROPERTIES && validate(value, ticket[value]));
 }
 
 const checkTicketList = (ticketList) => {
-    let valid = !(ticketList === undefined || ticketList === null);
-    for (let ticket of ticketList) {
-        valid = valid && checkTicket(ticket);
-    }
-    return valid;
+    return exists(ticketList) && ticketList.every((ticket) => checkTicket(ticket));
 }
 
 const checkIdNumber = (idNumber) => {
-    let valid = !(idNumber === undefined || idNumber === null)
-      && (idNumber > 0) && (idNumber < Number.MAX_SAFE_INTEGER);
+    return exists(idNumber) && (idNumber > 0) && (idNumber < Number.MAX_SAFE_INTEGER);
+}
+
+const checkTimestamp = (value) => {
+    return (value > ((-1) * DATE.MAX) && (value < DATE.MAX))
+}
+
+const checkTicketNumber = (value) => {
+    return (value > 0) && (value < Number.MAX_SAFE_INTEGER);
+}
+
+const checkUserType = (value) => {return value in USER_TYPE;}
+
+const checkStatus = (value) => {return value in STATUS;}
+
+const checkBoolean = (value) => {return value in [true, false];}
+
+const checkTicketViewMode = (value) => {return value in TICKET_VIEW;}
+
+export const isValid = (item) => {
+    let valid = exists(item) && 'type' in item && exists(item.type) && 'data' in item && exists(item.data);
+    if (valid) {
+        switch (item.type) {
+            case 'user': {
+                valid = checkUser(item.data);
+                break;
+            }
+            case 'username': {
+                valid = checkUsername(item.data);
+                break;
+            }
+            case 'email', 'userEmail': {
+                valid = checkEmail(item.data);
+                break;
+            }
+            case 'ticket': {
+                valid = checkTicket(item.data);
+                break;
+            }
+            case 'tickets': {
+                valid = checkTicketList(item.data);
+                break;
+            }
+            case 'timestamp': {
+                valid = checkTimestamp(item.data);
+                break;
+            }
+            case 'ticket_updates': {
+                valid = checkTicketUpdateList(item.data);
+                break;
+            }
+            case 'ticketUpdate': {
+                valid = checkTicketUpdate(item.data);
+                break;
+            }
+            case 'note', 'memo', 'details', 'ticket_issue': {
+                valid = checkMemo(item.data);
+                break;
+            }
+            case 'first', 'last', 'ticket_issue_title': {
+                valid = checkName(item.data);
+                break;
+            }
+            case 'contactPreference': {
+                valid = checkContact(item.data);
+                break;
+            }
+            case 'entryPermission': {
+                valid = checkEntry(item.data);
+                break;
+            }
+            case 'property', 'location': {
+                valid = checkProperty(item.data);
+                break;
+            }
+            case 'number', 'unit_number': {
+                valid = checkUnitNumber(item.data);
+                break;
+            }
+            case 'ticket_number': {
+                valid = checkTicketNumber(item.data);
+                break;
+            }
+            case 'type': {
+                valid = checkUserType(item.data);
+                break;
+            }
+            case 'status': {
+                valid = checkStatus(item.data);
+                break;
+            }
+            case 'emergency', 'activate', 'edit_mode': {
+                valid = checkBoolean(item.data);
+                break;
+            }
+            case 'password': {
+                valid = checkPassword(item.data);
+                break;
+            }
+            case 'phone': {
+                valid = checkPhone(item.data);
+                break;
+            }
+            case 'ticket_view_mode': {
+                valid = checkTicketViewMode(item.data);
+                break;
+            }
+            case 'units': {
+                valid = checkUnitList(item.data);
+                break;
+            }
+            default: break;
+        }
+    }
     return valid;
 }
 
@@ -232,7 +347,7 @@ export const validate = (property, value) => {
         'unit_number': ((item) => {return REGEX.UNIT_NUMBER.test(item)}),
         'email': ((item) => {return checkEmail(item)}),
         'username': ((item) => {return checkEmail(item)}),
-        'user': ((item) => {return checkEmail(item)}),
+        'userEmail': ((item) => {return checkEmail(item)}),
         'emergency': ((item) => {return item in [true, false]}),
         'activate': ((item) => {return item in [true, false]}),
         'edit_mode': ((item) => {return item in [true, false]}),
